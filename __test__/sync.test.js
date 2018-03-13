92684d0f64d1cd652fa276098927531b56186c65
Visal .In <invisal@gmail.com>
2018-03-13 16:36:28 +0700

    Fix text case to make it work with batch update.

--- __test__/sync.test.js
+++ __test__/sync.test.js
@@ -37,8 +37,9 @@ test('test sync between mysql and elastic search', async () => {
       port: config.mysql_port,
     },
     elastic: { host: config.es_host },
-    index: ({ row }) => ({ index: 'test_user', type: 'user', id: row.id, body: row }),
-    update: ({ row }) => ({ index: 'test_user', type: 'user', id: row.after.id, body: row.after }),
+    batch: 1,
+    index: ({ row }) => ({ action: 'index', index: 'test_user', type: 'user', id: row.id, body: row }),
+    update: ({ row }) => ({ action: 'update', index: 'test_user', type: 'user', id: row.after.id, body: row.after }),
     delete: ({ row }) => ({ action: 'delete', index: 'test_user', type: 'user', id: row.id }),
     success: () => { },
     error: () => { },
@@ -47,7 +48,7 @@ test('test sync between mysql and elastic search', async () => {
   s.start({ startAtEnd: true }, () => { });
 
   // Wait 2 seconds for our engine to properly start
-  await new Promise(resolve => setTimeout(resolve, 2000));
+  await new Promise(resolve => setTimeout(resolve, 3000));
 
   // Let make some changes
   await connection.query('INSERT INTO test_user VALUES(1, "Jonh", "M", "CEO", 1200)');
@@ -85,4 +86,4 @@ test('test sync between mysql and elastic search', async () => {
   expect(r2._source).toEqual({ id: 2, name: 'Mike', gender: 'M', title: 'CTO', salary: 1100 });
   expect(r3._source).toEqual({ id: 6, name: 'Sopheak', gender: 'M', title: 'Programmer', salary: 1000 });
 
-}, 10000);
+}, 15000);
